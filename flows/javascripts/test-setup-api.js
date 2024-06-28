9ce2f6049ec61b369378a54f1de12ccd45d6b32d
kathf <fleischerkath@gmail.com>
2024-06-28 13:21:13 +1000

    Allow finer-grained test record setup with traits

--- flows/javascripts/test-setup-api.js
+++ flows/javascripts/test-setup-api.js
@@ -1,10 +1,10 @@
 const baseUrl = (TEST_SETUP_API_BASE_URL === 'null') ? TEST_SETUP_API_BASE_URL_DEFAULT: TEST_SETUP_API_BASE_URL;
 const testSetupUrl = `${baseUrl}/api/v2/test_setup`;
 
-var create = function (recordName, type) {
+var create = function (recordName, type, traits) {
   return http.post(`${testSetupUrl}/${recordName}`, {
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ type: type}),
+    body: JSON.stringify({ type: type, traits: traits }),
   });
 };
 
--- flows/javascripts/test-user-create.js
+++ flows/javascripts/test-user-create.js
@@ -1,6 +1,12 @@
 function createTestUser() {
-  var createUserResponse = output.create("members", "cypress_borrower");
-  var responseBody = json(createUserResponse.body)
+  const traits = [
+    "with_sandbox_stripe_account",
+    "with_member_preference"
+  ]
+
+  const createUserResponse = output.create("members", "approved_member_with_applicant", traits);
+
+  const responseBody = json(createUserResponse.body)
   console.log(`Test member created: ${responseBody.email}`);
   return responseBody;
 };
